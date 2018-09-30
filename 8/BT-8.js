4e16a4b0d163df35725e47c10017cb662403e856
HeoBayMau <hieu.trung.030197@gmail.com>
2018-09-30 13:21:18 +0700

    Commit

--- 8/BT-8.js
+++ 8/BT-8.js
@@ -5,6 +5,7 @@ console.log("Enter number: " + number_b1_1);
 console.log("=> Absolute value: " + Math.abs(number_b1_1));
 console.log("Enter number: " + number_b1_2);
 console.log("=> Absolute value: " + Math.abs(number_b1_2));
+
 //Bai 2
 var number_b2_1 = 5;
 var number_b2_2 = 24;
@@ -18,6 +19,7 @@ if(number_b2_2 % 2 == 0)
     console.log("=> " + number_b2_2 + " is an even number");
 else
     console.log("=> " + number_b2_2 + " is an odd number");
+
 //Bai 3
 var hour = 3;
 var minute = 2;
@@ -26,6 +28,7 @@ console.log("Enter hour: " + hour);
 console.log("Enter minute: " + minute);
 console.log("Enter second: " + second);
 console.log("=> Number of seconds: " + ((hour * 60 * 60) + (minute * 60) + second));
+
 //Bai 4
 var x = 3, y = 2, z = 7, t = 1;
 console.log("Enter x: " + x);
@@ -44,6 +47,7 @@ for(var i = 0; i < arr.length; i++)
 }
 console.log("=> Minimum value: " + min);
 console.log("=> Maximum value: " + max);
+
 //Bai 5
 var month = 5;
 console.log("Enter month (1-12): " + month);
@@ -57,6 +61,7 @@ else
             console.log("We are in fall");
         else
             console.log("We are in winter");
+
 //Bai 6
 var year = 2000;
 console.log("Enter year: " + year);
@@ -84,3 +89,128 @@ for(var i = 1996; i <= year; i+=2)
 if(string.charAt(string.length) == ',')
     string.split(string, string.length - 1);
 console.log("Events in " + year + ": " + string);
+
+//Bai 7
+var day = 8;
+var month = 2;
+var year = 2015;
+
+
+//Bai 8
+for(var i = 1; i <= 5; i++)
+{
+    console.log("wwww.stdio.vn");
+}
+
+//Bai 9
+for(var i = 1; i <= 3; i++)
+{
+    switch(i % 3)
+    {
+        case 1:
+            console.log("www.stdio.vn");
+            break;
+        case 2:
+            console.log("bugs.vn");
+            break;
+        case 0:
+            console.log("training.stdio.vn");
+            break;
+    }
+}
+
+//Bai 10
+var n = 9;
+var s = 0;
+for(var i = 1; i <= n; i ++)
+{
+    s+= i;
+}
+console.log("S = " + s);
+
+//Bai 11
+var n = 5;
+var s = 1;
+console.log("Enter n: " + n);
+for(var i = 1; i <= n; i++)
+{
+    s *= i;
+}
+console.log("n! = " + s);
+
+//Bai 12
+var n = 5;
+var s = 0;
+for(var i = 1; i <= n; i+=2)
+{
+    s += i;
+}
+console.log("=> S = " + s);
+
+//Bai 13
+var n = 13;
+var prime = true;
+console.log("Enter number: " + n);
+if(n <= 1)
+{
+    prime = false;
+}
+else
+{
+    for(var i = 2; i < n; i++)
+    {
+        if(n % i == 0)
+        {
+            prime = false;
+            break;
+        }
+    }
+}
+if(prime == true)
+    console.log(n + " is prime");
+else
+    console.log(n + "is not prime");
+
+//Bai 14
+var n = 10;
+var arr = [];
+console.log("Enter number: " + n);
+for(var i = 1; i <= n; i++)
+{
+    if(n % i == 0)
+    {
+        arr.push(i);
+    }
+}
+var s = "Divisors of " + n + " are "; 
+for(var i = 0; i < arr.length; i++)
+{
+    s += arr[i].toString();
+    s += ", ";
+}
+console.log(s);
+
+//Bai 15
+var n = 6;
+var arr = [];
+console.log("Enter number: " + n);
+for(var i = 1; i < n; i++)
+{
+    if(n % i == 0)
+    {
+        arr.push(i);
+    }
+}
+var s = 0;
+for(var i = 0; i < arr.length; i++)
+{
+    s += arr[i];
+}
+if(s == n)
+{
+    console.log(n + " is a perfect number");
+}
+else
+{
+    console.log(n + " is not a perfect number");
+}
